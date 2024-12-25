0c1c23904f034a4cb9046d71d582722fbcf23437
thai19052001 <dinhthai1905@gmail.com>
2024-12-26 00:36:27 +0700

    before merge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,13 +22,10 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
@@ -44,30 +41,107 @@ export default {
           },
         },
       },
+      colors: {
+        primary: {
+          DEFAULT: "#4361ee",
+          light: "#eaf1ff",
+          "dark-light": "rgba(67,97,238,.15)",
+        },
+        secondary: {
+          DEFAULT: "#805dca",
+          light: "#ebe4f7",
+          "dark-light": "rgb(128 93 202 / 15%)",
+        },
+        success: {
+          DEFAULT: "#00ab55",
+          light: "#ddf5f0",
+          "dark-light": "rgba(0,171,85,.15)",
+        },
+        danger: {
+          DEFAULT: "#e7515a",
+          light: "#fff5f5",
+          "dark-light": "rgba(231,81,90,.15)",
+        },
+        warning: {
+          DEFAULT: "#e2a03f",
+          light: "#fff9ed",
+          "dark-light": "rgba(226,160,63,.15)",
+        },
+        info: {
+          DEFAULT: "#2196f3",
+          light: "#e7f7ff",
+          "dark-light": "rgba(33,150,243,.15)",
+        },
+        dark: {
+          DEFAULT: "#3b3f5c",
+          light: "#eaeaec",
+          "dark-light": "rgba(59,63,92,.15)",
+        },
+        black: {
+          DEFAULT: "#0e1726",
+          light: "#e3e4eb",
+          "dark-light": "rgba(14,23,38,.15)",
+        },
+        white: {
+          DEFAULT: "#ffffff",
+          light: "#e0e6ed",
+          dark: "#888ea8",
+        },
+      },
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+        googleSans: ["Google Sans", "sans-serif"],
+      },
+      spacing: {
+        4.5: "18px",
+      },
+      boxShadow: {
+        "3xl":
+          "0 2px 2px rgb(224 230 237 / 46%), 1px 6px 7px rgb(224 230 237 / 46%)",
+      },
+      typography: ({ theme }: any) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-invert-headings": theme("colors.white.dark"),
+            "--tw-prose-invert-links": theme("colors.white.dark"),
+            h1: { fontSize: "40px", marginBottom: "0.5rem", marginTop: 0 },
+            h2: { fontSize: "32px", marginBottom: "0.5rem", marginTop: 0 },
+            h3: { fontSize: "28px", marginBottom: "0.5rem", marginTop: 0 },
+            h4: { fontSize: "24px", marginBottom: "0.5rem", marginTop: 0 },
+            h5: { fontSize: "20px", marginBottom: "0.5rem", marginTop: 0 },
+            h6: { fontSize: "16px", marginBottom: "0.5rem", marginTop: 0 },
+            p: { marginBottom: "0.5rem" },
+            li: { margin: 0 },
+            img: { margin: 0 },
+          },
+        },
+      }),
     },
   },
-  plugins: [require("tailwindcss-animate")],
-  addVariablesForColors,
-  function({ matchUtilities, theme }: any) {
-    matchUtilities(
-      {
-        "bg-grid": (value: any) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-grid-small": (value: any) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-          )}")`,
-        }),
-        "bg-dot": (value: any) => ({
-          backgroundImage: `url("${svgToDataUri(
-            `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-          )}")`,
-        }),
-      },
-      { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
-    );
-  },
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-grid": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+    addVariablesForColors,
+  ],
 } satisfies Config;
